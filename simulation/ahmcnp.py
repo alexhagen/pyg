8cabf0459c68e845682fde7fd95298dd9d1f0af0
Alex Hagen <ahagen@purdue.edu>
2014-11-22 19:24:17 -0500

    starting a mcnp python library...should be able to absorb cagg into this repo later

---
+++ simulation/ahmcnp.py
@@ -0,0 +1,12 @@
+''' mcnp input decks can be programmed in an object oriented
+way to narrow the scope possible and thus to keep better
+agreement of parameters between problems '''
+
+class mcnp(object):
+    def __init__(self):
+        # initialize here!
+        pass;
+    def __del__(self):
+        # destroy here!
+        pass;
+    
