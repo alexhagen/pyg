3473fa810616e634097441aba27addf48f8b48de
Alex Hagen <ahagen@purdue.edu>
2015-05-14 09:46:07 -0400

    moved math.py to func.py and added stuff to the bash_rc

--- calc/func.py
+++ calc/func.py
@@ -1,15 +1,4 @@
-def import_non_local(name, custom_name=None):
-    import imp, sys
-
-    custom_name = custom_name or name
-
-    f, pathname, desc = imp.find_module(name, sys.path[1:])
-    module = imp.load_module(custom_name, f, pathname, desc)
-    f.close()
-
-    return module
-
-pymath = import_non_local('math','std_math');
+import math
 import numpy as np
 from ahpy.plotting import twod as ahp
 from scipy import nanmean
--- ne/ndata.py
+++ ne/ndata.py
@@ -1,39 +1,6 @@
 import numpy as np;
 import math;
-'''
-class matl(object):
-	def __init__(self,nuclides={}):
-		self.nuclides=nuclides;
 
-# decay:  https://t2.lanl.gov/nis/data/endf/decayVII.1.html
-# photon: https://t2.lanl.gov/nis/data/endf/endfvii-g.html
-# neutron: https://t2.lanl.gov/nis/data/endf/endfvii-n.html
-class nuclide(object):
-	name = '';
-	t_1_2 = None;
-	d_const = None;
-	
-
-class ndata(object):
-	nuclide = '';
-	txtfile = '';
-	nuclides = {};
-	possible_nuclides = [];
-	def __init__(self):
-		# make a folder for the data files
-	def add_component(self,nuclide,wo=None,ndens=None):
-		# add to the dict with the specified weight percent or number density
-	def export_mcnp_entry(self):
-		# print out an entry for a material card in mcnp
-	def t_1_2(self):
-		# check if single component
-		# return half life if so
-	def d_const(self):
-		# get half life
-		# return decay constant
-	def xs(self,rxn='N,N',E=None):
-		# return the cross section for a specified reaction at energies E or self.E
-'''
 # Helper Functions
 
 def read_endf_line(line):
