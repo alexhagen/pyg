46e1b2ebe2ef77be78be25c72aa47443be2192fa
Alex Hagen <ahagen@purdue.edu>
2015-10-30 13:15:18 -0400

    working towards pep8

--- twod.py
+++ twod.py
@@ -1,23 +1,23 @@
-#import modules
 from scipy.optimize import curve_fit
 from scipy.odr import *
 from math import exp
-#set svg as export
 import matplotlib
 import string
 import os
-from matplotlib.patches import Ellipse,Polygon
+from matplotlib.patches import Ellipse, Polygon
 from colour import Color
-matplotlib.use('pgf');
-import matplotlib.pyplot as plt
-plt.close("all")
 import numpy as np
+matplotlib.use('pgf')
+import matplotlib.pyplot as plt
 
-#make the line graphing class
+plt.close("all")
+
+
+# make the line graphing class
 class ah2d(object):
     """ Object ``ah2d`` is an object that holds a plot instance using the styles
         defined by ``ah_py``."""
-    leg=False;
+    leg = False
     leg_col_one_col = 2
     leg_col_two_col = 3
     leg_col_full_page = 4
@@ -29,33 +29,34 @@ class ah2d(object):
               5: '3',
               6: '4'}
     linestyle = {0: '-',
-            1: '--',
-            2: '-.',
-            3: ':'};
+                 1: '--',
+                 2: '-.',
+                 3: ':'}
     sizestring = {'1': 'onecolumn',
-            '2': 'twocolumn',
-            'fp': 'fullpage',
-            'cs': 'customsize',
-            'none': ''};
-    def __init__(self,env='plot'):
-        self.fig = plt.figure();
-        self.ax = self.fig.add_subplot(111);
-        self.ax_subp = [];
-        self.ax2 = None;
-        self.ax.spines['top'].set_visible(False);
-        self.ax.spines['right'].set_visible(False);
-        self.ax.get_xaxis().tick_bottom();
-        self.ax.get_yaxis().tick_left();
-        self.artists = [];
-        self.landscape = True;
-        self.width = 3.25;
-        self.height = self.width/1.61803398875;
-        self.plotnum = 0;
-        self.regnum = 0;
-        self.lines = {};
-        self.bars = {};
-        self.regs = {};
-        self.reg_string = {};
+                  '2': 'twocolumn',
+                  'fp': 'fullpage',
+                  'cs': 'customsize',
+                  'none': ''}
+
+    def __init__(self, env='plot'):
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
+        self.ax_subp = []
+        self.ax2 = None
+        self.ax.spines['top'].set_visible(False)
+        self.ax.spines['right'].set_visible(False)
+        self.ax.get_xaxis().tick_bottom()
+        self.ax.get_yaxis().tick_left()
+        self.artists = []
+        self.landscape = True
+        self.width = 3.25
+        self.height = self.width / 1.61803398875
+        self.plotnum = 0
+        self.regnum = 0
+        self.lines = {}
+        self.bars = {}
+        self.regs = {}
+        self.reg_string = {}
         if env is 'plot':
             rcparamsarray = {
                 "pgf.texsystem": "lualatex",
@@ -64,16 +65,16 @@ class ah2d(object):
                 "font.size": 8.0,
                 "axes.linewidth": 0.5,
                 "axes.edgecolor": "#746C66",
-                "xtick.major.width" : 0.25,
-                "xtick.major.size" : 2,
-                "xtick.direction" : "in",
-                "xtick.minor.width" : 0.125,
+                "xtick.major.width": 0.25,
+                "xtick.major.size": 2,
+                "xtick.direction": "in",
+                "xtick.minor.width": 0.125,
                 "xtick.color": "#746C66",
-                "ytick.major.width" : 0.25,
-                "ytick.major.size" : 2,
-                "ytick.minor.width" : 0.125,
+                "ytick.major.width": 0.25,
+                "ytick.major.size": 2,
+                "ytick.minor.width": 0.125,
                 "ytick.color": "#746C66",
-                "ytick.direction" : "in",
+                "ytick.direction": "in",
                 "text.color": "#746C66",
                 "axes.facecolor": "none",
                 "figure.facecolor": "none",
@@ -83,14 +84,14 @@ class ah2d(object):
                 "axes.labelsize": "medium",
                 "legend.fontsize": "small",
                 "legend.frameon": False,
-                "axes.grid"     : False,
-                "grid.color"    : "#A7A9AC",   # grid color
+                "axes.grid": False,
+                "grid.color": "#A7A9AC",   # grid color
                 "grid.linestyle": ":",       # dotted
                 "grid.linewidth": 0.125,     # in points
-                "grid.alpha"    : 0.5,     # transparency, between 0.0 and 1.0
-                "savefig.transparent" : True,
-                "path.simplify" : True,
-                "pgf.preamble" : "\usepackage{nicefrac}"
+                "grid.alpha": 0.5,     # transparency, between 0.0 and 1.0
+                "savefig.transparent": True,
+                "path.simplify": True,
+                "pgf.preamble": "\usepackage{nicefrac}"
             }
         elif env is 'gui':
             rcparamsarray = {
@@ -100,16 +101,16 @@ class ah2d(object):
                 "font.size": 8.0,
                 "axes.linewidth": 0.5,
                 "axes.edgecolor": "#746C66",
-                "xtick.major.width" : 0.25,
-                "xtick.major.size" : 2,
-                "xtick.direction" : "in",
-                "xtick.minor.width" : 0.125,
+                "xtick.major.width": 0.25,
+                "xtick.major.size": 2,
+                "xtick.direction": "in",
+                "xtick.minor.width": 0.125,
                 "xtick.color": "#746C66",
-                "ytick.major.width" : 0.25,
-                "ytick.major.size" : 2,
-                "ytick.minor.width" : 0.125,
+                "ytick.major.width": 0.25,
+                "ytick.major.size": 2,
+                "ytick.minor.width": 0.125,
                 "ytick.color": "#746C66",
-                "ytick.direction" : "in",
+                "ytick.direction": "in",
                 "text.color": "#746C66",
                 "axes.facecolor": "none",
                 "figure.facecolor": "none",
@@ -119,26 +120,28 @@ class ah2d(object):
                 "axes.labelsize": "medium",
                 "legend.fontsize": "small",
                 "legend.frameon": False,
-                "axes.grid"     : False,
-                "grid.color"    : "#A7A9AC",   # grid color
+                "axes.grid": False,
+                "grid.color": "#A7A9AC",   # grid color
                 "grid.linestyle": ":",       # dotted
                 "grid.linewidth": 0.125,     # in points
-                "grid.alpha"    : 0.5,     # transparency, between 0.0 and 1.0
-                "savefig.transparent" : True,
-                "path.simplify" : True,
-                "pgf.preamble" : "\usepackage{nicefrac}"
+                "grid.alpha": 0.5,     # transparency, between 0.0 and 1.0
+                "savefig.transparent": True,
+                "path.simplify": True,
+                "pgf.preamble": "\usepackage{nicefrac}"
             }
-        matplotlib.rcParams.update(rcparamsarray);
-    def xlabel(self,label,axes=None):
+        matplotlib.rcParams.update(rcparamsarray)
+
+    def xlabel(self, label, axes=None):
         """ ``ah2d.xlabel`` adds a label to the x-axis of the current axes (or
         other axis given by kwarg ``axes``).  The label can take LaTeX arguments
         and the ah style guide asks for labels given as 'Label ($variable$)
         [$unit$]'."""
         if axes is None:
-            axes = self.ax;
-        xlab=axes.set_xlabel(label);
-        self.artists.append(xlab);
-    def add_subplot(self,subp=121):
+            axes = self.ax
+        xlab = axes.set_xlabel(label)
+        self.artists.append(xlab)
+
+    def add_subplot(self, subp=121):
         """ ``ah2d.add_subplot`` follows Matlab's lead and allows you to plot
         several axes on one plot.  If kwarg ``subp`` is not defined, the default
         is to add a second plot in a 1x2 array.  When ``subp`` is defined, it
@@ -146,103 +149,122 @@ class ah2d(object):
         three columns and you are plotting in the 4th postition ``(2,1)``). The
         newly created axes is saved as ``ah2d.ax2`` - this should be expanded
         for more axes later."""
-        gsstr = str(subp);
-        gs1 = int(gsstr[0]);
-        gs2 = int(gsstr[1]);
-        self.ax.change_geometry(gs1,gs2,1);
-        self.ax2.change_geometry(gs1,gs2,1);
-        self.ax_subp.append(self.fig.add_subplot(subp));
-    def title(self,title):
-        ttl=self.ax.set_title(title);
-        self.artists.append(ttl);
-    def ylabel(self,label,axes=None):
+        gsstr = str(subp)
+        gs1 = int(gsstr[0])
+        gs2 = int(gsstr[1])
+        self.ax.change_geometry(gs1, gs2, 1)
+        self.ax2.change_geometry(gs1, gs2, 1)
+        self.ax_subp.append(self.fig.add_subplot(subp))
+
+    def title(self, title):
+        ttl = self.ax.set_title(title)
+        self.artists.append(ttl)
+
+    def ylabel(self, label, axes=None):
         if axes is None:
-            axes = self.ax;
-        ylab=axes.set_ylabel(label);
-        self.artists.append(ylab);
-    def xlim(self,minx,maxx,axes=None):
-        self.ax.set_xlim([minx,maxx]);
-    def ylim(self,miny,maxy,axes=None):
-        self.ax.set_ylim([miny,maxy]);
-    def legend(self,loc=1):
-        self.leg = self.ax.legend(loc=loc);
-        (legobjs,legtitles) = self.ax.get_legend_handles_labels();
-        inc_objs = [];
-        inc_titles = [];
-        for i in range(0,len(legtitles)):
+            axes = self.ax
+        ylab = axes.set_ylabel(label)
+        self.artists.append(ylab)
+
+    def xlim(self, minx, maxx, axes=None):
+        self.ax.set_xlim([minx, maxx])
+
+    def ylim(self, miny, maxy, axes=None):
+        self.ax.set_ylim([miny, maxy])
+
+    def legend(self, loc=1):
+        self.leg = self.ax.legend(loc=loc)
+        (legobjs, legtitles) = self.ax.get_legend_handles_labels()
+        inc_objs = []
+        inc_titles = []
+        for i in range(0, len(legtitles)):
             if 'connector' not in legtitles[i]:
-                inc_objs.append(legobjs[i]);
-                inc_titles.append(legtitles[i]);
-        self.ax.legend(inc_objs,inc_titles);
-    def xticks(self,ticks,labels,axes=None):
+                inc_objs.append(legobjs[i])
+                inc_titles.append(legtitles[i])
+        self.ax.legend(inc_objs, inc_titles)
+
+    def xticks(self, ticks, labels, axes=None):
         if axes is not None:
-            plt.sca(axes);
+            plt.sca(axes)
         else:
-            plt.sca(self.ax);
-        plt.xticks(ticks,labels);
-    def yticks(self,ticks,labels,axes=None):
+            plt.sca(self.ax)
+        plt.xticks(ticks, labels)
+
+    def yticks(self, ticks, labels, axes=None):
         if axes is not None:
-            plt.sca(axes);
+            plt.sca(axes)
         else:
-            plt.sca(self.ax);
-        plt.yticks(ticks,labels);
+            plt.sca(self.ax)
+        plt.yticks(ticks, labels)
+
     def markers_on(self):
         for key in self.lines:
             self.lines[key].set_alpha(1.0)
             self.lines[key].set_markersize(6)
+
     def markers_off(self):
         for key in self.lines:
             self.lines[key].set_markersize(0)
+
     def lines_on(self):
         for key in self.lines:
             self.lines[key].set_linewidth(1.0)
+
     def lines_off(self):
         for key in self.lines:
             self.lines[key].set_linewidth(0.0)
-    def add_vline(self,x,ymin,ymax,ls='solid',lw=1.5,color='black'):
-        return plt.vlines(x,ymin,ymax,linestyles=ls,linewidths=lw,color=color);
-    def add_hline(self,y,xmin=None,xmax=None,ls='solid',lw=1.5,color='black'):
-        return plt.hlines(y,xmin=xmin,xmax=xmax,linestyle=ls,linewidth=lw,color=color);
-    def add_label(self,x,y,string,color='black'):
-        curve_place = (x,y);
-        self.ax.annotate(string,
-                        xy=curve_place,
-                        xytext=curve_place,color=color);
-    def add_data_pointer(self,x,curve=None,point=None,string=None,
-                         place='up-right',axes=None):
-        if axes is None:
-            axes = self.ax;
 
+    def add_vline(self, x, ymin, ymax, ls='solid', lw=1.5, color='black'):
+        return plt.vlines(x, ymin, ymax, linestyles=ls, linewidths=lw,
+                          color=color)
+
+    def add_hline(self, y, xmin=None, xmax=None, ls='solid', lw=1.5,
+                  color='black'):
+        return plt.hlines(y, xmin=xmin, xmax=xmax, linestyle=ls, linewidth=lw,
+                          color=color)
+
+    def add_label(self, x, y, string, color='black'):
+        curve_place = (x, y)
+        self.ax.annotate(string,
+                         xy=curve_place,
+                         xytext=curve_place, color=color)
+
+    def add_data_pointer(self, x, curve=None, point=None, string=None,
+                         place='up-right', axes=None):
+        if axes is None:
+            axes = self.ax
         if curve is not None:
-            y = curve.at(x);
+            y = curve.at(x)
         elif point is not None:
-            y = point;
+            y = point
         else:
-            raise Exception('No point for the arrow given in reference to ' + \
-                'data pointer.');
+            raise Exception('No point for the arrow given in reference to ' +
+                            'data pointer.')
         if string is None:
-            string = '$\left( %f,%f \\right)$' % (x,y);
+            string = '$\left( %f,%f \\right)$' % (x, y)
         if place == 'up-right':
-            curve_place = (4.0*x/3.0,4.0*y/3.0);
+            curve_place = (4.0 * x / 3.0, 4.0 * y / 3.0)
         elif place == 'up-left':
-            curve_place = (3.0*x/4.0,4.0*y/3.0);
+            curve_place = (3.0 * x / 4.0, 4.0 * y / 3.0)
         elif place == 'down-right':
-            curve_place = (4.0*x/3.0,3.0*y/4.0);
+            curve_place = (4.0 * x / 3.0, 3.0 * y / 4.0)
         elif place == 'down-left':
-            curve_place = (2.0*x/4.0,3.0*y/4.0);
+            curve_place = (2.0 * x / 4.0, 3.0 * y / 4.0)
         elif type(place) is tuple:
-            curve_place = place;
+            curve_place = place
         axes.annotate(string,
-                        xy=(x,y),
-                        xytext=curve_place,
-                        arrowprops=dict(arrowstyle="fancy",
-                                        fc="0.3",ec="none",
-                                        patchB = Ellipse((2, -1), 0.5, 0.5),
-                                        connectionstyle = \
-                                            "angle3,angleA=0,angleB=-90")
-                        )
-    def add_reg_line(self,x,y,regtype='lin',name='reg',xerr=None,yerr=None):
-        self.regnum = self.regnum+1;
+                      xy=(x, y),
+                      xytext=curve_place,
+                      arrowprops=dict(arrowstyle="fancy",
+                                      fc="0.3", ec="none",
+                                      patchB=Ellipse((2, -1), 0.5, 0.5),
+                                      connectionstyle=
+                                      "angle3,angleA=0,angleB=-90")
+                      )
+
+    def add_reg_line(self, x, y, regtype='lin', name='reg', xerr=None,
+                     yerr=None):
+        self.regnum = self.regnum + 1
         if name is 'reg':
             name = 'reg%d' % (self.regnum);
         # set up the error bounds
@@ -325,10 +347,11 @@ class ah2d(object):
             downerrlines = plt.plot(x_fit,y_err_down,color='#D1D3D4',ls='--');
             self.ax.fill_between(x_fit,y_err_up,y_err_down,facecolor='#D1D3D4',alpha=0.5,lw=0.0);
             # add the regression to the dict of regressions
-    def fill_between(self,x,y1,y2,fc='red',name='plot',axes=None):
+
+    def fill_between(self, x, y1, y2, fc='red', name='plot', axes=None):
         if axes is None:
-            axes = self.ax;
-        self.plotnum=self.plotnum+1;
+            axes = self.ax
+        self.plotnum = self.plotnum + 1
         if name is 'plot':
             name = 'plot%d' % (self.plotnum);
         axes.fill_between(x,y1,y2,facecolor=fc,alpha=0.2,linewidth=0.0);
@@ -386,32 +409,39 @@ class ah2d(object):
         maxi = calfunc(np.max(self.ax.get_xlim()));
         self.ax2.set_xlim(mini,maxi);
         self.ax2.get_xaxis().tick_top();
-    def add_hist(self,y,bins,name='plot'):
-        self.plotnum=self.plotnum+1;
+
+    def add_hist(self, y, bins, name='plot'):
+        self.plotnum = self.plotnum + 1
         if name is 'plot':
             name = 'plot%d' % (self.plotnum)
-        n,bins,patches=plt.hist(y,bins=bins,label=name,facecolor='gray',alpha=0.5,normed=False);
-        self.bars[name] = patches;
-        return n,bins;
-    def add_bar(self,x,y,name='plot'):
-        self.plotnum=self.plotnum+1;
+        n, bins, patches = plt.hist(y, bins=bins, label=name, facecolor='gray',
+                                    alpha=0.5, normed=False)
+        self.bars[name] = patches
+        return n, bins
+
+    def add_bar(self, x, y, name='plot'):
+        self.plotnum = self.plotnum + 1
         if name is 'plot':
             name = 'plot%d' % (self.plotnum)
-        delta = [j-i for i, j in zip(x[:-1], x[1:])];
-        delta.append(delta[-1]);
-        #x = [j - (i/2) for i, j in zip(delta, x)];
-        patches=plt.bar(x,y,width=delta,label=name,facecolor='gray',alpha=0.5);
-        self.bars[name] = patches;
-        return x,y,delta;
+        delta = [j - i for i, j in zip(x[:-1], x[1:])]
+        delta.append(delta[-1])
+        # x = [j - (i/2) for i, j in zip(delta, x)];
+        patches = plt.bar(x, y, width=delta, label=name, facecolor='gray',
+                          alpha=0.5)
+        self.bars[name] = patches
+        return x, y, delta
+
     def add_legend(self):
-        self.leg=True
-        leg = self.ax.legend();
-        self.artists.append(leg);
+        self.leg = True
+        leg = self.ax.legend()
+        self.artists.append(leg)
+
     def det_height(self):
         if self.landscape:
-            self.height = self.width/1.61803398875;
+            self.height = self.width / 1.61803398875
         else:
-            self.height = self.width*1.61803398875;
+            self.height = self.width * 1.61803398875
+
     def remove_font_sizes(self,filename):
         f=open(filename,'r')
         fstring = "\\centering \n" + f.read()
@@ -422,6 +452,7 @@ class ah2d(object):
         fstring = filter(lambda x: x in string.printable, fstring);
         f.write(fstring)
         f.close()
+
     def long_name(self):
         self.leg_col_one_col = 1
         self.leg_col_two_col = 1
@@ -479,7 +510,11 @@ class ah2d(object):
                 bbox_extra_artists=self.artists,bbox_inches='tight');
         if format is 'pgf':
             self.remove_font_sizes(filename+self.sizestring[size]+add);
-    def export(self,filename,sizes=['1'],formats=['pgf'],customsize=None,legloc=None):
+
+    def export(self, filename, sizes=['1'], formats=['pgf'],
+               customsize=None, legloc=None):
+        # writing a comment here to make the following commented code not
+        # docstring
         '''
         # remove all points outside the window
         for key in self.lines:
@@ -498,5 +533,6 @@ class ah2d(object):
         '''
         for size in sizes:
             for format in formats:
-                self.set_size(size,len(sizes),customsize=customsize,legloc=legloc);
-                self.export_fmt(filename,size,len(sizes),format);
+                self.set_size(size, len(sizes), customsize=customsize,
+                              legloc=legloc)
+                self.export_fmt(filename, size, len(sizes), format)
