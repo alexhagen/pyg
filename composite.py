e8d4b58da7fc4fa4de99fc07d06677529df22b30
Alex Hagen <ahagen@purdue.edu>
2017-08-07 14:05:19 -0400

    working location - I think. Pretty bad code, need to clean it up and work on the margins code

--- composite.py
+++ composite.py
@@ -7,7 +7,7 @@ import svgutils.compose as sc
 from sys import platform
 import subprocess
 
-gscale = 1.00
+gscale = 0.96
 
 class asset(object):
 	def __init__(self, filename, bbox, parent):
@@ -15,18 +15,17 @@ class asset(object):
 		self.bbox = bbox
 		self.parent = parent
 		self.cwd = os.getcwd()
-		self.gscale = 1.5
+		self.gscale = 1.144117591
 
 	def calc_pos(self):
 		width = self.parent.get_width(self.filename)
 		height = self.parent.get_height(self.filename)
 		aspect_ratio = width / height
-		dpi = 96.
 		bbox = self.parent.ax.transData.transform(self.bbox)
-		print bbox
-		max_width = (bbox[1, 0] - bbox[0, 0])/self.gscale
-		max_height = (bbox[1, 1] - bbox[0, 1])/self.gscale
-		# Our first option is to make the width the full of the bounding box
+		max_width = np.abs(bbox[1, 0] - bbox[0, 0])/self.gscale
+		max_height = np.abs(bbox[1, 1] - bbox[0, 1])/self.gscale
+		self.fig_height = self.parent.fig.dpi * self.parent.fig.get_figheight() / self.gscale
+		top_margin = self.fig_height - self.parent.get_height(self.parent.svg_filename) - self.parent.get_y(self.parent.svg_filename)
 		set_width = max_width
 		set_height = set_width * aspect_ratio
 		self.scale = (set_width / width)
@@ -34,21 +33,9 @@ class asset(object):
 			set_height = max_height
 			set_width = set_height / aspect_ratio
 			self.scale = (set_height / height)
-		self.x = ((bbox[0, 0] + bbox[1, 0])/2.0 - (set_width/2.0))/self.gscale
-		self.y = ((bbox[1, 1] + bbox[0, 1])/2.0 + (set_height/2.0))/self.gscale
-		#print scale
-		#print disp_coords
-		#print fig_coords
-		#self.scale = 1.0
-		print "set_width: %f set_height: %f" % (set_width, set_height)
-		print "width: %f height: %f" % (width, height)
-		print "scale: %f" % (self.scale)
-		print "fig_width: %f" % (self.parent.fig.dpi * self.parent.fig.get_figwidth())
-		print "fig_height: %f" % (self.parent.fig.dpi * self.parent.fig.get_figheight())
-		self.fig_height = gscale * self.parent.fig.dpi * self.parent.fig.get_figheight()
-		self.y = self.fig_height - self.y
-		#self.x = (self.x - (set_width / 2.0))/1.25
-		print "pos: %f, %f" % (self.x, self.y)
+		self.x = ((bbox[0, 0] + bbox[1, 0])/2.0 - (set_width/2.0))/self.gscale - self.parent.get_x(self.parent.svg_filename)
+		self.y = ((bbox[1, 1] + bbox[0, 1])/2.0 + (set_height/2.0))/self.gscale - top_margin
+		self.y = self.parent.get_height(self.parent.svg_filename) - self.y
 
 class composite(twod.pyg2d):
 	def __init__(self):
@@ -117,7 +104,6 @@ class composite(twod.pyg2d):
 		p = subprocess.Popen([cmd], stdout=subprocess.PIPE,
 							 stderr=subprocess.PIPE, shell=True)
 		(out, err) = p.communicate()
-		#print out,err
 		y = float(out)
 		return y
 
@@ -129,7 +115,7 @@ class composite(twod.pyg2d):
 		x2 = pos[0] + wh[0]/2.0
 		y1 = pos[1] - wh[1]/2.0
 		y2 = pos[1] + wh[1]/2.0
-		self.add_line([x1, x1, x2, x2, x1], [y1, y2, y2, y1, y1])
+		#self.add_line([x1, x1, x2, x2, x1], [y1, y2, y2, y1, y1])
 		bbox = np.array([[x1, y1], [x2, y2]])
 		asst = asset(filename, bbox, self)
 		if above:
@@ -138,7 +124,9 @@ class composite(twod.pyg2d):
 			self.below.extend([asst])
 
 	def export(self, *args, **kwargs):
-		super(composite, self).export(*args, formats=['svg'], **kwargs)
+		super(composite, self).export(*args, formats=['websvg'], **kwargs)
+		os.system('inkscape -z --export-area-drawing --export-pdf=temp.pdf %s' % (self.svg_filename))
+		os.system('inkscape -z --export-plain-svg=%s temp.pdf' % (self.svg_filename))
 		below_panels = []
 		for asst in self.below:
 			asst.calc_pos()
@@ -146,7 +134,7 @@ class composite(twod.pyg2d):
 				.move(asst.x, asst.y))
 			below_panels.extend([x])
 		x = sc.Panel(sc.SVG(self.svg_filename))
-		x.moveto(0., 0., gscale)
+		x.scale(gscale)
 		panel = [x]
 		above_panels = []
 		for asst in self.above:
@@ -155,7 +143,6 @@ class composite(twod.pyg2d):
 				.move(asst.x, asst.y))
 			above_panels.extend([x])
 		panels = below_panels + panel + above_panels
-		print panels
 		sc.Figure("%fpx" % (self.get_width(self.svg_filename)),
 				  "%fpx" % (self.get_height(self.svg_filename)),
 			*panels
