eb902159a465ec65933a1d6d050ca5196a48747d
Alex Hagen <alexhagen6@gmail.com>
2018-09-13 09:44:06 -0700

    Added pnnl tints

--- pyg/colors/pnnl.py
+++ pyg/colors/pnnl.py
@@ -18,17 +18,7 @@ pnnl_colors = {'black': '#000000',
                'teal': '#66B6CD',
                'red': '#BE0F34',
                'copper': '#D77600',
-               'copper80': '#DF9453',
-               'copper70': '#E2A169',
-               'copper60': '#E7AF7E',
-               'copper50': '#EBBB93',
-               'copper40': '#F0C9AA',
                'silver': '#616265',
-               'silver80': '#818284',
-               'silver70': '#919294',
-               'silver60': '#A0A1A3',
-               'silver50': '#B0B1B3',
-               'silver40': '#C1C1C1',
                'bronze': '#A63F1E',
                'gold': '#F4AA00',
                'platinum': '#B3B3B3',
@@ -40,8 +30,79 @@ pnnl_colors = {'black': '#000000',
                'topaz': '#0081AB',
                'amethyst': '#502D7F',
                'garnet': '#870150',
-               'emslgreen': '#719500'
-               }
+               'emslgreen': '#719500'}
+# Now we need to add the tints - note that these may not match exactly the
+# tints on reputation.pnl.gov - however they are computed correctly
+pnnl_colors.update({'copper40': '#967040',
+                    'copper50': '#A17136',
+                    'copper60': '#AC722B',
+                    'copper70': '#B77320',
+                    'copper80': '#C17415',
+                    'silver40': '#3B4F8B',
+                    'silver50': '#314A94',
+                    'silver60': '#28459E',
+                    'silver70': '#1E40A8',
+                    'silver80': '#143BB2',
+                    'bronze40': '#894E3B',
+                    'bronze50': '#934931',
+                    'bronze60': '#9D4427',
+                    'bronze70': '#A73F1D',
+                    'bronze80': '#B03A14',
+                    'gold40': '#AB8D49',
+                    'gold50': '#B7923D',
+                    'gold60': '#C39731',
+                    'gold70': '#CF9C25',
+                    'gold80': '#DCA018',
+                    'platinum40': '#D19595',
+                    'platinum50': '#D98D8D',
+                    'platinum60': '#E18585',
+                    'platinum70': '#E87E7E',
+                    'platinum80': '#F07676',
+                    'onyx40': '#111C27',
+                    'onyx50': '#0E1C2A',
+                    'onyx60': '#0B1C2D',
+                    'onyx70': '#081C30',
+                    'onyx80': '#061C32',
+                    'emerald40': '#24543A',
+                    'emerald50': '#1E5A39',
+                    'emerald60': '#186038',
+                    'emerald70': '#126638',
+                    'emerald80': '#0C6C37',
+                    'sapphire40': '#2B3F63',
+                    'sapphire50': '#233D6A',
+                    'sapphire60': '#1C3B72',
+                    'sapphire70': '#153979',
+                    'sapphire80': '#0E3780',
+                    'ruby40': '#8F3D4F',
+                    'ruby50': '#9A3349',
+                    'ruby60': '#A42943',
+                    'ruby70': '#AE1F3D',
+                    'ruby80': '#B81437',
+                    'mercury40': '#68A0BE',
+                    'mercury50': '#5EA4C9',
+                    'mercury60': '#53A7D4',
+                    'mercury70': '#48AADF',
+                    'mercury80': '#3DADE9',
+                    'topaz40': '#336778',
+                    'topaz50': '#2B6B80',
+                    'topaz60': '#227089',
+                    'topaz70': '#1A7491',
+                    'topaz80': '#11789A',
+                    'amethyst40': '#513478',
+                    'amethyst50': '#502B81',
+                    'amethyst60': '#4E228A',
+                    'amethyst70': '#4D1A92',
+                    'amethyst80': '#4C119B',
+                    'garnet40': '#5F2949',
+                    'garnet50': '#66224A',
+                    'garnet60': '#6D1B4B',
+                    'garnet70': '#74144D',
+                    'garnet80': '#7A0E4E',
+                    'emslgreen40': '#5A682D',
+                    'emslgreen50': '#5E7025',
+                    'emslgreen60': '#62771E',
+                    'emslgreen70': '#657F16',
+                    'emslgreen80': '#69860F'})
 c = pnnl_colors
 
 start_c = _c(pnnl_colors["silver"])
@@ -78,44 +139,3 @@ for s, l in zip(np.linspace(0.0, end_c.saturation, 128),
     second_half.append(deepcopy(__color__))
 #first_half = [copy(start_c).set_saturation(num) for num in np.linspace(start_c.saturation, 0.0, 128)]
 jet_cmap = first_half + second_half
-'''
-black = pu_colors['black']
-gray = pu_colors['gray']
-gray_dark = pu_colors['darkgray']
-gray_darker = pu_colors['browngray']
-gray_light = pu_colors['tan']
-gray_lighter = pu_colors['lightgray']
-brand_primary = pu_colors['newgold']
-brand_success = pu_colors['green']
-brand_info = pu_colors['teal']
-brand_warning = pu_colors['yellow']
-brand_error = pu_colors['red']
-brand_off = pu_colors['purple']
-
-
-desat = _c(pu_colors["blue"])
-desat.saturation = 0.0
-desat.luminance = 0.1
-flow_cmap = list(desat.range_to(_c(pu_colors["blue"]), 256))
-desat = _c(pu_colors["newgold"])
-desat.saturation = 0.0
-desat.luminance = 0.1
-brand_cmap = list(desat.range_to(_c(pu_colors["newgold"]), 256))
-desat = _c(pu_colors["red"])
-desat.saturation = 0.0
-desat.luminance = 0.75
-flame_cmap = list(desat.range_to(_c(pu_colors["red"]), 256))
-desat = _c(pu_colors["teal"])
-desat.saturation = 0.0
-desat.luminance = 0.9
-teal_cmap = list(desat.range_to(_c(pu_colors["teal"]), 256))
-desat = _c(pu_colors["white"])
-bw_cmap = list(desat.range_to(_c(pu_colors["black"]), 256))
-
-start_c = _c(pu_colors["blue"])
-mid_c = _c(pu_colors["green"])
-pu_jet_cmap = list(start_c.range_to(mid_c, 128)) + list(mid_c.range_to(c(pu_colors["red"]), 128))
-
-start_c = _c(pu_colors["darkgray"])
-brand_cmap = list(start_c.range_to(_c(pu_colors["newgold"]), 256))
-'''
